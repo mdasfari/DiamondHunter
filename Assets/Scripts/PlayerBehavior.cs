f89fa4dda669bd054294ab9386896114c5fb00c6
Mohammad ASFARi <mdasfari@hotmail.com>
2023-06-15 16:33:52 +0300

    Level 1 outline created

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
 {
+    public int PlayerSpeed = 7;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,10 @@ public class PlayerBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float hInput = Input.GetAxis("Horizontal");
+
+        Debug.Log("hInput: " + hInput);
+
+        transform.position = new Vector3(hInput + Time.deltaTime * PlayerSpeed, 0);
     }
 }
