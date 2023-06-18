b396865b2c25938b47f8363f8de568dfc0d9a343
Mohammad ASFARi <mdasfari@hotmail.com>
2023-06-18 16:44:51 +0300

    Added Menus

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void ResumeGame()
+    {
+
+    }
+
+    public void QuitToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -4,21 +4,47 @@ using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
 {
+    private SpriteRenderer spriteRender;
+    private Animator animator;
+    private Rigidbody2D rb;
     public int PlayerSpeed = 7;
+    public int PlayerJumpForce = 5;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRender = GetComponent<SpriteRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        bool jumpState = false;
+        if (Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("Jump!!!!");
+            // rb.AddForce(Vector2.up * PlayerJumpForce, ForceMode2D.Impulse);
+            jumpState = true;
+        }
+
+
+        animator.SetBool("Jumping", jumpState);
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), jumpState ? PlayerJumpForce : 0);
+
+        if (moveInput.x > 0.01f)
+            spriteRender.flipX = false;
+        else if (moveInput.x < 0.01f)
+            spriteRender.flipX = true;
+
+        rb.velocity = moveInput * PlayerSpeed; // .normalized * PlayerSpeed;
+
+        animator.SetBool("Running", (moveInput.x != 0));
     }
 
     // Update is called once per frame
     void Update()
     {
-        float hInput = Input.GetAxis("Horizontal");
 
-        Debug.Log("hInput: " + hInput);
-
-        transform.position = new Vector3(hInput + Time.deltaTime * PlayerSpeed, 0);
     }
 }
