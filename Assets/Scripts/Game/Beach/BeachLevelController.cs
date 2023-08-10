7e7aa725d8ceb202c1aa9c814ee54ee621ee2ef5
Alextsuicl <134810226+Alextsuicl@users.noreply.github.com>
2023-08-09 22:18:32 -0400

    Revert "Added Code Comment - for IntroState, BeachLevelController, StartRuinsLevel"
    This reverts commit e0607b04b83dd47a3c8f7214024601fb2165ca8c.

--- Assets/Scripts/Game/Beach/BeachLevelController.cs
+++ Assets/Scripts/Game/Beach/BeachLevelController.cs
@@ -4,37 +4,32 @@ using UnityEngine;
 
 public class BeachLevelController : MonoBehaviour
 {
-    // Section for intro settings.
     [Header("Intro")]
 
-    // Section for animation settings.
     [Header("Animation Settings")]
-    public Transform shipObject; // Reference to the Transform component of the ship object.
-    public Transform playerObject; // Reference to the Transform component of the player object.
-    public float SailSpeed = 20f; // The speed at which the ship sails.
+    public Transform shipObject;
+    public Transform playerObject;
+    public float SailSpeed = 20f;
 
-    // The final positions for the ship and player objects.
     public Vector2 shipEndPosition = new Vector2(20.07f, 0.14f);
     public Vector2 playerEndPosition = new Vector2(-9.7f, -3.237f);
 
-    private FiniteStateMachine stateMachine; // Reference to the Finite State Machine for controlling game states.
-
-    // Called before the first frame update; initializes the state machine.
+    private FiniteStateMachine stateMachine;
+    // Start is called before the first frame update
     void Start()
     {
-        IntroState introState = new IntroState(this); // Create an instance of the IntroState class.
-        stateMachine = new FiniteStateMachine(introState); // Initialize the state machine with the intro state.
+        IntroState introState = new IntroState(this);
+        stateMachine = new FiniteStateMachine(introState);
     }
 
-    // Called once per frame; handles game logic updates.
+    // Update is called once per frame
     private void Update()
     {
-        stateMachine.LogicUpdate(); // Update the logic of the current state in the state machine.
+        stateMachine.LogicUpdate();
     }
 
-    // Called at a fixed interval; handles physics-related updates.
     private void FixedUpdate()
     {
-        stateMachine.PhysicsUpdate(); // Update the physics of the current state in the state machine.
+        stateMachine.PhysicsUpdate();
     }
 }
