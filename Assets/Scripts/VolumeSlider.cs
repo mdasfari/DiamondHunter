cf410ee272b66370934bce08b3f24cbf2bc6e070
kathleen <catlean4@gmail.com>
2023-09-11 14:22:47 -0600

    volume slider script

---
+++ Assets/Scripts/VolumeSlider.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeSlider : MonoBehaviour
+{
+    public Slider volumeSlider;
+    void Start()
+        {
+
+            if (!PlayerPrefs.HasKey("Music"))
+            {
+                PlayerPrefs.SetFloat("Music", 1);
+                Load();
+            }
+            else
+            {
+                Load();
+            }
+        }
+
+    // Update is called once per frame
+    void Update()
+        {
+        
+        }
+    public void ChangeVolume()
+        {
+            AudioListener.volume = volumeSlider.value;
+            Save();
+        }
+    public void Save()
+        {
+            PlayerPrefs.SetFloat("Music",volumeSlider.value);
+        }
+    public void Load()
+        {
+            volumeSlider.value = PlayerPrefs.GetFloat("Music");
+            AudioListener.volume = volumeSlider.value;
+        }
+}
