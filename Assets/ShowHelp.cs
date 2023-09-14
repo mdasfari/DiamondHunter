3ca904d5f57829281d1ac64c71b6f98524daa274
Mohammad ASFARi <mdasfari@hotmail.com>
2023-09-14 23:58:35 +0300

    Intrduce the help popup system

---
+++ Assets/ShowHelp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowHelp : MonoBehaviour
+{
+    public Canvas helpCanvas;
+    public Transform showLocation;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        helpCanvas.enabled = true;
+        //helpCanvas.transform.position = Vector2.up * 100;
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        helpCanvas.enabled = false;
+        //helpCanvas.transform.position = Vector2.up * -100;
+    }
+
+}
