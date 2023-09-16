24bb2ec7559a77a866e88044c29c35e2492a3984
kathleen <catlean4@gmail.com>
2023-09-16 14:15:48 -0600

    popup message fuctionality

---
+++ Assets/Scripts/Messages/MessageTrigger.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MessageTrigger : MonoBehaviour
+{
+    public static MessageTrigger _instance;
+    public TextMeshProUGUI textComponent;
+
+    public void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    public void Start()
+    {
+        Cursor.visible = true;
+        gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void SetAndShowToolTip(string message)
+    {
+        gameObject.SetActive(true);
+        textComponent.text = message;
+    }
+
+    public void HideToolTip()
+    {
+        gameObject.SetActive(false);
+        textComponent.text = string.Empty;
+    }
+}
---
+++ Assets/Scripts/Messages/Tooltip.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tooltip : MonoBehaviour
+{
+    public string message;
+
+    private void OnMouseEnter()
+    {
+        MessageTrigger._instance.SetAndShowToolTip(message);
+    }
+
+    private void OnMouseExit()
+    {
+        MessageTrigger._instance.HideToolTip();
+    }
+}
