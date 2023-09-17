2c094520656bc921b0bcd45a415a7125832347c5
Alextsuicl <134810226+Alextsuicl@users.noreply.github.com>
2023-09-17 00:22:00 -0400

    Added Lore-Sign Trigger with Player Movement

--- Assets/Scripts/Messages/Tooltip.cs
+++ Assets/Scripts/Messages/Tooltip.cs
@@ -3,16 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Tooltip : MonoBehaviour
+
 {
     public string message;
+    public float detectionRadius = 0.5f; // Adjust as needed
+    private bool isPlayerNear = false;
 
-    private void OnMouseEnter()
+    private void Update()
     {
-        MessageTrigger._instance.SetAndShowToolTip(message);
+        Collider2D playerCollider = Physics2D.OverlapCircle(transform.position, detectionRadius, LayerMask.GetMask("Player"));
+        if (playerCollider && !isPlayerNear)
+        {
+            isPlayerNear = true;
+            MessageTrigger._instance.SetAndShowToolTip(message);
+        }
+        else if (!playerCollider && isPlayerNear)
+        {
+            isPlayerNear = false;
+            MessageTrigger._instance.HideToolTip();
+        }
     }
 
-    private void OnMouseExit()
-    {
-        MessageTrigger._instance.HideToolTip();
-    }
-}
