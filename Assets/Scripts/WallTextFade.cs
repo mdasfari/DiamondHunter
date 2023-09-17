fbb119ae200d4f0dd45e6b1f17b1d566c258dc7e
Alextsuicl <134810226+Alextsuicl@users.noreply.github.com>
2023-09-17 11:17:36 -0400

    Add a text fade effect (only appear when mouseover)

---
+++ Assets/Scripts/WallTextFade.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+using System.Collections;
+
+
+[RequireComponent(typeof(CanvasGroup))]
+public class WallTextFade : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private CanvasGroup canvasGroup;
+    public float fadeSpeed = 2f;
+
+    private void Awake()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+        canvasGroup.alpha = 0; // Start with the text invisible
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        StopAllCoroutines();
+        StartCoroutine(FadeTo(1f)); // Fade in
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        StopAllCoroutines();
+        StartCoroutine(FadeTo(0f)); // Fade out
+    }
+
+    private IEnumerator FadeTo(float targetAlpha)
+    {
+        while (!Mathf.Approximately(canvasGroup.alpha, targetAlpha))
+        {
+            canvasGroup.alpha = Mathf.MoveTowards(canvasGroup.alpha, targetAlpha, fadeSpeed * Time.deltaTime);
+            yield return null;
+        }
+    }
+}
