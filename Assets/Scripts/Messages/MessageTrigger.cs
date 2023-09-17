03658ef000e56ffdbfee25c27ff491f7299995ed
Alextsuicl <134810226+Alextsuicl@users.noreply.github.com>
2023-09-17 00:22:35 -0400

    Changed Pop-up to float around mouse

--- Assets/Scripts/Messages/MessageTrigger.cs
+++ Assets/Scripts/Messages/MessageTrigger.cs
@@ -8,6 +8,10 @@ public class MessageTrigger : MonoBehaviour
     public static MessageTrigger _instance;
     public TextMeshProUGUI textComponent;
 
+    private float angle = 0f;
+    private float radius = 50f; // distance from the cursor
+    private float speed = 2f; // speed of rotation
+
     public void Awake()
     {
         if (_instance != null && _instance != this)
@@ -28,7 +32,12 @@ public class MessageTrigger : MonoBehaviour
 
     void Update()
     {
-        transform.position = Input.mousePosition;
+        angle += speed * Time.deltaTime;
+
+        float x = Input.mousePosition.x + radius * Mathf.Cos(angle);
+        float y = Input.mousePosition.y + radius * Mathf.Sin(angle);
+
+        transform.position = new Vector3(x, y, transform.position.z);
     }
 
     public void SetAndShowToolTip(string message)
@@ -43,3 +52,4 @@ public class MessageTrigger : MonoBehaviour
         textComponent.text = string.Empty;
     }
 }
+
